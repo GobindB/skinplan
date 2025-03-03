8ecdfd337edc66d84832d035e1f8262cf49a6d8e
Gobind <gobind@thesinghs.org>
2025-03-02 16:52:27 -0800

    :gear: add analytics code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import { Analytics } from "@vercel/analytics/react"
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,6 +17,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <html lang="en" data-oid="5p53:aw">
             <body className={inter.className} data-oid="bf3bkif">
                 {children}
+                <Analytics />
             </body>
         </html>
     );
